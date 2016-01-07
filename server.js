af766e6cda14c59e181b7346b1d19713781bae60
Fabrice LEFEBVRE <fabrice.lefebvre@atos.net>
2016-01-07 15:30:04 +0100

    trying mongoose...

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var mongoose = require("mongoose");
 var app = express();
 var router = express.Router();
 var path = __dirname + '/views/';
@@ -9,7 +10,41 @@ router.use(function (req,res,next) {
 });
 
 router.get("/",function(req,res){
-  res.sendFile(path + "index.html");
+    mongoose.connect('mongodb://localhost:27017/db');
+    var db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'connection error:'));
+
+    var operationSchema = new mongoose.Schema({
+       operationType: String
+    });
+    var transactionSchema = new mongoose.Schema({
+        amount: Number, 
+        exponent: Number,
+        operations: [operationSchema]
+    });
+    var TransactionModel = mongoose.model('transactions', transactionSchema);
+    var OperationModel = mongoose.model('operation', operationSchema);
+
+    /*var myTransaction1 = new TransactionModel({amount: 1200, exponent: 2});
+    var myTransaction2 = new TransactionModel({amount: 2000, exponent: 1, });
+
+    myTransaction2.save(function(err) {
+        if(err) {throw err;}
+        console.log('Transaction2 saved');
+        mongoose.connection.close();
+    });*/
+    
+    TransactionModel.find(null, function(err, transactions) {
+        if(err) throw err;
+        console.log('Transactions: ' + transactions);
+    })
+
+    OperationModel.find(null, function(err, operations) {
+        if(err) throw err;
+        console.log('Operations: ' + operations);
+    })
+
+    res.sendFile(path + "index.html");
 });
 
 app.use(express.static('public'));
